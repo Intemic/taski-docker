390faa1b58fc14c16e065255abd46193bb67fb67
Антон Лучик <Intemic@yandex.ru>
2023-08-03 18:58:25 +0500

    Add hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,21 +7,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-#SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
-
-#DEBUG = True
-
-#ALLOWED_HOSTS = ['127.0.0.1']
-
 SECRET_KEY = os.getenv('KITTY_KEY', default='None')
 
 DEBUG = os.getenv('DEBUG_MODE', False) == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost, 127.0.0.1').split(',')
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
